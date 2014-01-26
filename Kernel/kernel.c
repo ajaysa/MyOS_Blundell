82505a836ddc758047dcf1496ffafd2eefd31fd9
Ajay Saini <ajay_saini1006@yahoo.co.in>
2014-01-25 22:20:36 -0500

    Directory containing files related to our kernel

---
+++ Kernel/kernel.c
@@ -0,0 +1,10 @@
+void some_function()
+{
+}
+
+void main()
+{
+	char *video_memory = (char *)0xb8000;
+	*video_memory = 'X';
+	some_function();
+}
---
+++ Kernel/kernel_entry.asm
@@ -0,0 +1,6 @@
+[bits 32]
+[extern main]
+
+call main
+
+jmp $
