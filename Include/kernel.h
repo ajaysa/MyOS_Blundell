4b737457804c031b7413342e5933bbbfee3c705d
Ajay Saini <ajay_saini1006@yahoo.co.in>
2014-02-01 21:51:52 -0500

    Directory containing header files

---
+++ Include/kernel.h
@@ -0,0 +1,8 @@
+/* This defines what the stack looks like after an ISR was running */
+struct regs
+{
+    unsigned int gs, fs, es, ds;      /* pushed the segs last */
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
+    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
+    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */
+};
---
+++ Include/screen.h
@@ -0,0 +1,8 @@
+#define VIDEO_MEMORY 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLUMNS 80
+
+#define WHITE_ON_BLACK 0x0f
+
+#define SCREEN_CTRL_REG 0x3d4
+#define SCREEN_DATA_REG 0x3d5
