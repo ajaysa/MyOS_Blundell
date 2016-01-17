8d7b0e95f0d376381cb35ae5695962f5e1bf797b
Ajay Saini <ajay_saini1006@yahoo.co.in>
2016-01-17 00:13:21 -0800

    updated to use latest functionality

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,5 +1,9 @@
 #include <kernel.h>
 
+#ifndef _KHEAP
+#include <memory.h>
+#endif
+
 void some_function()
 {
 }
@@ -24,9 +28,62 @@ void main()
 	print("Hello World\n");
 
 	print("\n\tFrom Ajay Saini!\n");
+	print("\nHeap Memory SPEC :: \n");
+	kheap_SPEC();
+	print("\n\n");
 
 	print(_CMD);
 
+	// scanf testing
+/*	print("Enter a Number :: \n");
+	scan('d', &num);
+*/
+	// heap memory test
+/*	char *test, *temp;
+	int i;
+	test = (char *)kmalloc(10);
+
+	if ( !test )
+		print("memory allocation failed\n");
+	else
+	{
+		temp = test;
+		print("memory allocated at :: ");
+		print_hex(temp);
+
+		for(i=0; i<10; i++)
+		{
+			*temp='A';
+			temp++;
+		}
+
+		temp = test;
+		for(i=0; i<10; i++)
+		{
+			printchar(*temp);
+			temp++;
+		}
+
+		temp = NULL;
+		kfree(test);
+	}
+*/
 	// test : running of exception handlers
 	//print(20/0);
+
+	// check memory map
+/*	char *addr = (char *)0x5000000;
+	do
+	{
+		printchar(*addr);
+		*addr = '9';
+		addr++;
+	}while(addr < 0x5000100);
+
+	addr = (char *)0x5000000;
+	do
+	{
+		printchar(*addr);
+		addr++;
+	}while(addr < 0x5000100); */
 }
